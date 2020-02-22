cb683ae95ce2dd7866040c2c23d8f0cd68ddd5f4
MursalimAnsari <42885623+MursalimAnsari@users.noreply.github.com>
2020-02-22 20:05:17 +0530

    added comment

--- src/main/java/com/university/solutions/UniversityManagementApplication.java
+++ src/main/java/com/university/solutions/UniversityManagementApplication.java
@@ -2,7 +2,11 @@ package com.university.solutions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * @author Mursalim
+ *  This class is the entry point for the university management api
+ *  22/feb/2020
+ */
 @SpringBootApplication
 public class UniversityManagementApplication {
 
